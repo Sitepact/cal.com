16fd84d186f348d1bf0a9449d7acacff31836c80
Ryon Whyte <ryonwhyte@gmail.com>
2025-08-28 00:04:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const next = require('next');
 
 const app = next({ dev: false, dir: './apps/web' });
 const handle = app.getRequestHandler();
-const PORT = process.env.PORT || 3009;
+
+const PORT = process.env.PORT || 3000; // <-- important
 
 app.prepare().then(() => {
-  createServer((req, res) => handle(req, res)).listen(PORT, '127.0.0.1', () => {
+  createServer((req, res) => handle(req, res)).listen(PORT, '0.0.0.0', () => {
     console.log(`Cal.com running on ${PORT}`);
   });
 });
