29b2c2c1f7b4a960dcd96ee99be508b4cb044525
Ryon Whyte <ryonwhyte@gmail.com>
2025-08-28 10:01:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-// app.js
 const { createServer } = require('http');
 const path = require('path');
 
 let next;
-try {
-  next = require('next'); // works if hoisted to root
-} catch {
-  next = require(path.join(__dirname, 'apps/web/node_modules/next')); // fallback
-}
+try { next = require('next'); }
+catch { next = require(path.join(__dirname, 'apps/web/node_modules/next')); }
 
 const app = next({ dev: false, dir: './apps/web' });
 const handle = app.getRequestHandler();
-
-const PORT = process.env.PORT || 3000; // Passenger sets this
+const PORT = process.env.PORT || 3000;
 
 app.prepare().then(() => {
   createServer((req, res) => handle(req, res)).listen(PORT, '0.0.0.0', () => {
